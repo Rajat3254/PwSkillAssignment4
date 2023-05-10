ddaac8c9f624f10ccf315a2a2af6fd8952b830b8
Rajat Singh <singhr3234@gmail.com>
2023-05-10 14:26:29 +0530

    question4 is added

---
+++ question_4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class question_4 {
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("enter the integer ");
+    int x=sc.nextInt();
+    if(x>0){
+        System.out.println(x+"is positive integer");
+    }else{
+        System.out.println("the number is negative so skip");
+    }
+
+
+        
+    }
+}
+    
+
